f2bf6856330c60f9d3b593ef91041d1feda9f3bf
Dillon Coffman <tug40394@temple.edu>
2019-10-31 16:39:42 -0400

    WebView working

--- app/src/main/java/edu/temple/networkops_10_31_19/MainActivity.java
+++ app/src/main/java/edu/temple/networkops_10_31_19/MainActivity.java
@@ -4,56 +4,53 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView htmlDisplay;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        htmlDisplay = findViewById(R.id.htmlDisplay);
+        webView = findViewById(R.id.webView);
 
         Handler displayHandler = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(Message msg) {
                 // message will be string, so it won't be inside what variable, instead inside obj variable
-                htmlDisplay.setText((String) msg.obj);
+                webView.loadData((String) msg.obj, "text/html", "UTF-8"); // 1st arg is content it should load, 2nd is mimetype, 3rd is encoding
                 return true;
             }
         });
 
         findViewById(R.id.goButton).setOnClickListener(v -> { // load content into TextView using Lambda function
-            new Thread() {
-                @Override
-                public void run() {
-                    URL url = null;
-                    try {
-                        url = new URL("https://www.temple.edu");
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-                        StringBuilder builder = new StringBuilder(); // StringBuilder, keep adding on bits of a string
-                        String response;
-                        while ((response = reader.readLine()) != null) {
-                            builder.append(response);
-                        }
-                        // Need to use Handler
-                        Message msg = Message.obtain();
-                        msg.obj = builder.toString(); // gives you string created from StringBuilder object
-                        displayHandler.sendMessage(msg);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
+            webView.loadUrl("https://www.temple.edu");
+            webView.setWebViewClient(new WebViewClient());
+//            new Thread() {
+//                @Override
+//                public void run() {
+//                    URL url = null;
+//                    try {
+//                        url = new URL("https://www.temple.edu");
+//                        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//                        StringBuilder builder = new StringBuilder(); // StringBuilder, keep adding on bits of a string
+//                        String response;
+//                        while ((response = reader.readLine()) != null) {
+//                            builder.append(response);
+//                        }
+//                        // Need to use Handler
+//                        Message msg = Message.obtain();
+//                        msg.obj = builder.toString(); // gives you string created from StringBuilder object
+//                        displayHandler.sendMessage(msg);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }.start();
         });
     }
 }
